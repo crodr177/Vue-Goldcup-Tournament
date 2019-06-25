21a29e7cd28cbe69ac0994344d33e4a3f0121bde
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2019-06-25 15:45:09 -0700

    added inputs for tables and brackets

--- src/components/Brackets.vue
+++ src/components/Brackets.vue
@@ -1,6 +1,59 @@
 <template>
   <div id="bracket-container">
+    <h2>Brackets</h2>
+    <main id="tournament">
+      <ul class="round round-2">
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top winner"><input maxlength="20" placeholder="1B Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom "><input maxlength="20" placeholder="2A Team"/> <span class="score"><input maxlength="1" placeholder="0"/></span></li>
 
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top winner"><input maxlength="20" placeholder="1A Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom "><input maxlength="20" placeholder="2B Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top "><input maxlength="20" placeholder="1C Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom winner"><input maxlength="20" placeholder="2D Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top "><input maxlength="20" placeholder="1D Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom winner"><input maxlength="20" placeholder="2C Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+
+          <li class="spacer">&nbsp;</li>
+      </ul>
+      <ul class="round round-3">
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top winner"><input maxlength="20" placeholder="Team 1B/2A"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom "><input maxlength="20" placeholder="Team 1A/2B"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top "><input maxlength="20" placeholder="Team 1C/2D"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom winner"><input maxlength="20" placeholder="Team 1D/2C"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+
+          <li class="spacer">&nbsp;</li>
+      </ul>
+      <ul class="round round-4">
+          <li class="spacer">&nbsp;</li>
+          
+          <li class="game game-top winner"><input maxlength="20" placeholder="Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          <li class="game game-spacer">&nbsp;</li>
+          <li class="game game-bottom "><input maxlength="20" placeholder="Team"/> <span class="score"><input maxlength="2" placeholder="0"/></span></li>
+          
+          <li class="spacer">&nbsp;</li>
+      </ul>       
+    </main>
   </div>
 </template>
 
@@ -11,5 +64,84 @@ export default {
 </script>
 
 <style>
+  main{
+  margin-top: 20px;
+  display:flex;
+  flex-direction:row;
+  background-color: white;
+  padding-left: 80px;
+  border-radius: 3px;
+  }
+  .round{
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    width:250px;
+    list-style:none;
+    padding:0;
+    font-size: 20px;
+  }
+  .round .spacer{ flex-grow:.5; }
+  .round .spacer:first-child,
+  .round .spacer:last-child{ flex-grow:.2; }
 
+  .round .game-spacer{
+    flex-grow:1;
+  }
+  #bracket-container{
+    padding:10px;
+    line-height:1.4em;
+  }
+
+  li.game{
+    padding-left:20px;
+  }
+
+  li.game input {
+    border: none;
+    font-size: 20px;
+    color: rgb(179, 151, 102);
+    border: 2px solid white;
+    outline: none;
+    border-radius: 3px;
+    width: 180px;
+  }
+
+  li.game input::placeholder {
+    font-style: italic;
+  }
+
+  li.game input:hover {
+    border: 2px solid black;
+  }
+
+  li.game .score input:hover {
+    border: 2px solid black;
+  }
+
+  li.game .score{
+    float:right;
+    margin-right:5px;
+    width: 35px;
+  }
+
+  li.game .score input {
+    width: 35px;
+    margin: 5px;
+    border: 2px solid white;
+    border-radius: 3px;
+    text-align: center;
+    color: rgb(12, 12, 173);
+  }
+
+  li.game-top{ border-bottom:1px solid black; }
+
+  li.game-spacer{ 
+    border-right:1px solid black;
+    min-height:40px;
+  }
+
+  li.game-bottom{ 
+    border-top:1px solid black;
+  }
 </style>
--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -1,89 +1,92 @@
 <template>
-  <div id="table-container">
-    <table>
-      <tr>
-        <th colspan="2">Group A</th>
-      </tr>
-      <tr>
-        <td>Mexico</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Canada</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Martinique</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Cuba</td>
-        <td>0</td>
-      </tr>
-    </table>
-     <table>
-      <tr>
-        <th colspan="2">Group B</th>
-      </tr>
-      <tr>
-        <td>Costa Rica</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Haiti</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Nicaragua</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Bermuda</td>
-        <td>0</td>
-      </tr>
-    </table>
-     <table>
-      <tr>
-        <th colspan="2">Group C</th>
-      </tr>
-      <tr>
-        <td>Honduras</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Jamaica</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>El Salvador</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Curacao</td>
-        <td>0</td>
-      </tr>
-    </table>
-     <table>
-      <tr>
-        <th colspan="2">Group D</th>
-      </tr>
-      <tr>
-        <td>USA</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Panama</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Trinidad &#38; Tobago</td>
-        <td>0</td>
-      </tr>
-      <tr>
-        <td>Guyana</td>
-        <td>0</td>
-      </tr>
-    </table>
+  <div>
+    <h2>Tables</h2>
+    <div id="table-container">
+      <table>
+        <tr>
+          <th colspan="2">Group A</th>
+        </tr>
+        <tr>
+          <td>Mexico</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Canada</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Martinique</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Cuba</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th colspan="2">Group B</th>
+        </tr>
+        <tr>
+          <td>Costa Rica</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Haiti</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Nicaragua</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Bermuda</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th colspan="2">Group C</th>
+        </tr>
+        <tr>
+          <td>Honduras</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Jamaica</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>El Salvador</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Curacao</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th colspan="2">Group D</th>
+        </tr>
+        <tr>
+          <td>USA</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Panama</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Trinidad &#38; Tobago</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+        <tr>
+          <td>Guyana</td>
+          <td class="points"><input maxlength="1" placeholder="0"/></td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -101,6 +104,13 @@ export default {
     justify-content: space-between;
     flex-wrap: wrap;
   }
+  
+  h2 {
+    color: white;
+    font-size: 30px;
+    margin-bottom: 0px;
+    margin-top: 50px;
+  }
 
   #table-container table {
     border-radius: 3px;
@@ -115,9 +125,24 @@ export default {
     text-align: left;
     font-size: 20px;
     font-weight: bolder;
+    padding-left: 5px;
   }
 
   #table-container td {
     border-top: 1px solid rgba(0, 0, 0, .2);
+    padding-left: 5px;
+  }
+
+  #table-container .points {
+     width: 50px;
+     padding-left: 0px;
+  }
+
+  #table-container .points input {
+    width: 50px;
+    border: 2px solid black;
+    border-radius: 3px;
+    font-size: 18px;
+    text-align: center;
   }
 </style>
